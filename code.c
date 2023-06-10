b50a1b34bda8add1e2999965eb7452c7a4628e28
Matheus Palma <matheus.oliveira@dock.tech>
2023-06-10 15:40:45 -0300

    Adding semaphore struct

--- code.c
+++ code.c
@@ -7,15 +7,43 @@
 #include <stdlib.h>
 #include "code.h"
 
+/********* Variable functions declarations */
+
+
 /********* Statics functions declarations */
 
-
+static void semaphoreConstructor(void);
+static void wait(void);
+static void signal(void);
 
 /********* Functios definitions*/
+
+semaphoreFunctions semaphore;
 
 int main(void)
 {
     LOG("Debug mode on!\n");
 
+    semaphoreConstructor();
+
+
     return 0;
+}
+
+static void semaphoreConstructor(void)
+{
+    memcpy(semaphore.signal, signal,sizeof(signal));
+    //semaphore.signal = &signal;
+    memcpy(semaphore.wait, wait, sizeof(wait));
+    //semaphore.wait = &wait;
+}
+
+static void wait(void)
+{
+    semaphore.control = semaphoreWait;
+}
+
+static void signal(void)
+{
+    semaphore.control = semaphoreSignal;
 }
