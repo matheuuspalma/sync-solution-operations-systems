a550df7fb1bff72c30c6fbafdbe239a1e703dca2
pedroRocha <pedrorocha@poli.ufrj.br>
2023-07-15 21:33:03 -0300

    version 1

--- code.c
+++ code.c
@@ -23,17 +23,23 @@ static int helpElves(void);
 static int getHelp(void);
 
 /********* Variable declarations */
+pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
+int finalized = 0;
+pthread_mutex_t mutex;
 
 christmasStruct santa, reindeers, elves;
 
-int processing = 0;
 
-/********* Functios definitions*/
+int processing = 1;
+
+/********* Main Function definition*/
 int main(void)
 {
+    
+    unsigned char index;
     pthread_t santaId;
-    pthread_t reindeersId;
-    pthread_t elvesId;
+    pthread_t reindeersId [NUMBER_OF_REINDEERS];
+    pthread_t elvesId[NUMBER_OF_ELVES];
 
     LOG("Debug mode on!\n");
 
@@ -42,59 +48,143 @@ int main(void)
     constructor(elves, 0);
 
     pthread_create(&santaId, NULL, (THREAD_FUNC_PTR)&santaThread, NULL);
-    pthread_create(&reindeersId, NULL,(THREAD_FUNC_PTR)&reindeersThread, NULL);
-    pthread_create(&elvesId, NULL, (THREAD_FUNC_PTR)&elvesThread, NULL);
 
-    sleep(20);
+    for(index = 0; index < NUMBER_OF_REINDEERS; index++){
+        pthread_create(&reindeersId[index], NULL,(THREAD_FUNC_PTR)&reindeersThread, NULL);
+    }
+
+    for(index = 0; index < NUMBER_OF_ELVES; index++){
+        pthread_create(&elvesId[index], NULL, (THREAD_FUNC_PTR)&elvesThread, NULL);
+    }
+    sleep(50);
 
     return 0;
 }
 
+/********* Threads Functios definitions*/
 static int santaThread(void)
 {
-    LOG("Santa's thread is running!\n");
-    LOG("Santa is sleeping");
+    pthread_mutex_init(&mutex, NULL);
 
+    printf("\n%sSanta's thread is running!%s\n", WHITE, RESET);
+    printf("\n%sSanta is sleeping%s\n",YELLOW, RESET);
+
+    printf("\nSanta's thread\n");
     while(processing)
     {
-
-        if( reindeers.counter == ALL_ELVES_RETURNED ||
-            elves.counter     == MINIMUN_ELVES_IN_TROUBLE)
+        finalized = 0;
+        if( reindeers.counter == ALL_ELVES_RETURNED)
         {
-            LOG("Santa should awake !!");
-            LOG("Elves should wait wait until after Christmas!!");
+            printf("\n%sSanta should awake !!%s\n", YELLOW, RESET);
+            printf("\n%sElves should wait wait until after Christmas!!%s\n", YELLOW, RESET);
             prepareSleigh();
         }
 
-        else if(reindeers.counter != ALL_ELVES_RETURNED ||
-        elves.counter     == MINIMUN_ELVES_IN_TROUBLE )
+        if(elves.counter >= MINIMUN_ELVES_IN_TROUBLE || reindeers.counter < ALL_ELVES_RETURNED )
         {
-            LOG("Santa should awake !!");
+            printf("\n%sSanta should awake !!%s\n", YELLOW, RESET);
             helpElves();
         }
         else
         {
-             LOG("Santa should keep resting !!");
+            printf("\n%sSanta should keep resting !!%s\n", YELLOW, RESET);
         }
+         sleep(200);
+
+        pthread_mutex_lock(&mutex);
+        while(finalized == 0)
+            pthread_cond_wait(&condition, &mutex);
+        pthread_mutex_unlock(&mutex);
     }
-    sleep(10);
     return OK;
 }
 
 static int reindeersThread(void)
 {
-    LOG("Reindeers' thread is running!\n");
-    getHitched();
+    printf("\n%sReindeers thread is running!%s\n", WHITE, RESET);
     sleep(10);
+    if(reindeers.counter == 9){
+            getHitched();
+        }
+
+    if(reindeers.counter < 9)
+    {
+        pthread_mutex_lock(&mutex);
+        finalized = 1;
+        pthread_cond_signal(&condition);
+        pthread_mutex_unlock(&mutex);
+    }
+
+    pthread_mutex_lock(&mutex);
+    reindeers.counter += 1;
+    printf("\n%sA reindeer arrived in the Pole!%s\n", BLUE, RESET);
+    finalized = 1;
+    pthread_cond_signal(&condition);
+    pthread_mutex_unlock(&mutex);
+    
     return OK;
 }
 
 static int elvesThread(void)
 {
-    LOG("Elves' thread is running!\n");
-    getHelp();
+    printf("\n%sElves' thread is running!%s\n", WHITE, RESET);
     sleep(10);
+    
+    if(reindeers.counter == 9){
+        pthread_mutex_lock(&mutex);
+        while(finalized == 0)
+            pthread_cond_wait(&condition, &mutex);
+        pthread_mutex_unlock(&mutex);
+    }
+
+    pthread_mutex_lock(&mutex);
+    finalized = getHelp();
+    pthread_cond_signal(&condition);
+    pthread_mutex_unlock(&mutex);
     return OK;
+}
+
+
+/********* Auxiliar Functios definitions*/
+static int prepareSleigh(void)
+{
+   
+    printf("\n%sPreparing sleigh!%s\n", BLUE, RESET);
+    sleep(2);
+    printf("\n%sDelivering gifts...%s\n", BLUE, RESET);
+    sleep(20);
+    reindeers.counter =- 9;
+    printf("\n%sThe reindeers go back to South Pacific!%s\n", BLUE, RESET);
+        
+
+    return 0;
+}
+
+static int getHitched(void)
+{
+    printf("%s\nSleigh Hitched!%s\n", BLUE, RESET);
+    //LOG("Sleigh Hitched");
+    sleep(5);    
+    return 0;
+}
+
+static int helpElves(void)
+{
+    
+    printf("\n%sHelping elves%s\n", RED, RESET);
+    elves.counter = elves.counter - 3;     
+    printf("\n%sA elf was helped!%s\n", YELLOW, RESET);
+    sleep(5);
+
+    return 0;
+}
+
+static int getHelp(void)
+{
+    elves.counter += 1;
+    printf("%s\nI need help (An Elf).%s\n", GREEN, RESET);
+    sleep(5);
+    return 1;
 }
 
 static void constructor(christmasStruct e, unsigned quantity)
@@ -103,22 +193,4 @@ static void constructor(christmasStruct e, unsigned quantity)
     e.lock = opened;
     e.counter = quantity;
     LOG("Construction sucessfull");
-}
-
-static int prepareSleigh(void)
-{
-    return 0;
-}
-static int getHitched(void)
-{
-    return 0;
-}
-
-static int helpElves(void)
-{
-    return 0;
-}
-static int getHelp(void)
-{
-    return 0;
 }
--- code.h
+++ code.h
@@ -1,6 +1,7 @@
 #ifndef _CODE_H_
 #define _CODE_H_
 
+
 #include <stdio.h>
 #include <string.h>
 
@@ -12,12 +13,14 @@
 
 #define LOG(...) do{ if(LOG_ON) {printf("%d:%s(): ", __LINE__, __func__ ); printf(__VA_ARGS__); printf("\n");}} while(0)
 
+
 typedef enum{
     closed = 0,
     opened
 }operations;
 
 typedef struct christmasFunctions_
+
 {
     operations lock;
     unsigned int counter;
@@ -31,6 +34,20 @@ typedef void * (*THREAD_FUNC_PTR)(void *);
 
 /* MACROS */
 
+#define RED					"\e[0;31m"
+#define BLACK					"\e[0;30m"
+#define RED					"\e[0;31m"
+#define GREEN					"\e[0;32m"
+#define BLUE					"\e[0;34m"
+#define YELLOW					"\e[0;33m"
+#define MAGENTA       				"\e[0;35m"
+#define CYAN					"\e[0;36m"
+#define WHITE					"\e[0;37m"
+#define RESET					"\e[0m"
+
+
+#define NUMBER_OF_REINDEERS      9
+#define NUMBER_OF_ELVES          10
 #define ALL_ELVES_RETURNED       9
 #define MINIMUN_ELVES_IN_TROUBLE 3
 #endif //_CODE_H
